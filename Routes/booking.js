03696907d347bb8edd7fee70aba2fe772f1512a0
Pedro Priori <kadilo1@hotmail.com>
2024-05-30 13:37:15 -0300

    add cancel booking

--- Routes/booking.js
+++ Routes/booking.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { authenticate } from './../auth/verifyToken.js'
-import { getCheckoutSession } from '../Controllers/bookingController.js'
+import { getCheckoutSession, deleteBooking } from '../Controllers/bookingController.js'
 
 const router = express.Router()
+router.use(authenticate)
 
-router.post('/checkout-session/:doctorId', authenticate, getCheckoutSession)
+router.post('/checkout-session/:doctorId', getCheckoutSession)
+router.delete('/bookings/:id', deleteBooking);
 
 export default router
