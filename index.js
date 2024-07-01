a372c7acc36eb6b0c70b2358cf79133f83830644
Pedro Priori <kadilo1@hotmail.com>
2024-07-01 16:04:35 -0300

    lazy commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import reviewRoute from './Routes/review.js';
 import bookingRoute from './Routes/booking.js'
 import appointmentRoute from './Routes/appointment.js'
 import webhookRoute from './Routes/webhook.js'
+import zoomRoute from './Routes/zoom.js'
 
 dotenv.config()
 
@@ -19,10 +20,6 @@ const port = process.env.PORT || 8000
 const corsOptions = {
   origin: true
 }
-
-app.get('/', (req, res) => {
-  res.send('Api is working')
-})
 
 // database connection
 mongoose.set('strictQuery', false)
@@ -35,9 +32,13 @@ const connectDB = async () => {
 
     console.log('MongoDB database is connected')
   } catch (error) {
-    console.log('MongoDB database is connection failed')
+    console.log('MongoDB database is connection failed', error)
   }
 }
+
+app.get('/', async (req, res) => {
+  res.send('Api is working')
+})
 
 // middleware
 app.use(express.json())
@@ -50,6 +51,7 @@ app.use('/api/v1/reviews', reviewRoute)
 app.use('/api/v1/bookings', bookingRoute)
 app.use('/api/v1/appointments', appointmentRoute)
 app.use('/api/v1/webhooks', webhookRoute)
+app.use('/api/v1/zoom', zoomRoute)
 
 app.listen(port, () => {
   connectDB();
